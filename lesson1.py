aa86de755fd4650fca1e64faa5a8885380827543
Ilnaz <sadykovilnaz096@gmail.com>
2024-10-17 15:59:58 +0500

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+name = 'Курс:'
+first_name = 'Python'
+str = 'всего задач:'
+int = 12
+second_name = 'затрачено часов:'
+hours_cout = 1.5
+three_name = 'среднее время выполнения:'
+times_cout = 0.125
+four_name = 'часа'
+print (name,first_name, str,int, second_name,hours_cout,three_name,times_cout,four_name)
+
